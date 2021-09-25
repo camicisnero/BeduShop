0944c7cf5a21f66c472a296109f9e45c33276e4f
franciscorgodoy <rubengodoy570@gmail.com>
2021-09-25 20:37:05 -0300

    added toolbar menu with options search and more info

--- app/src/main/java/org/bedu/bedushop/HomeActivity.kt
+++ app/src/main/java/org/bedu/bedushop/HomeActivity.kt
@@ -1,7 +1,13 @@
 package org.bedu.bedushop
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,11 +21,41 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val appBar = findViewById<Toolbar>(R.id.app_bar)
+        this.setSupportActionBar(appBar)
+
         bottomNavigationView = findViewById(R.id.nav_view)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.homeActivity_nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item_search->{
+                Toast.makeText(this,"La busqueda esta desabilitada", Toast.LENGTH_SHORT).show()
+            }
+            R.id.item_help->{
+                val url:String = "https://www.bedu.org"
+                openWebPage(url)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openWebPage(url: String) {
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
