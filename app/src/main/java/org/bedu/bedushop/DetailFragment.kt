0cd2d07ad675ba844e9449d437c51f625b9eb382
Camila <antocisnero@gmail.com>
2021-09-27 20:21:14 -0300

    Changes: change colorOnSecondary, add onClick listener to button on DetailFragment

--- app/src/main/java/org/bedu/bedushop/DetailFragment.kt
+++ app/src/main/java/org/bedu/bedushop/DetailFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import android.widget.*
 import androidx.navigation.fragment.navArgs
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
@@ -42,26 +40,10 @@ class DetailFragment : Fragment() {
         txtCalificationProduct = view.findViewById(R.id.tv_calification)
         imgProduct = view.findViewById(R.id.img_product)
         txtPriceCuotaProducto = view.findViewById(R.id.tv_priceCuotas)
+        btnAddToCart = view.findViewById(R.id.btn_add)
+
 
         super.onViewCreated(view, savedInstanceState)
-        /*arguments?.let{
-            //Without SafeArgs
-            val title = arguments?.getString("TITLE");
-            val price = arguments?.getFloat("PRICE");
-            val description = arguments?.getString("DESCRIPTION");
-            val valuation = arguments?.getFloat("VALUATION");
-            val calification = arguments?.getInt("CALIFICATION");
-            val image = arguments?.getString("IMAGE");
-            val quota = arguments?.getFloat("QUOTA")
-
-            txtNameProduct.text = title
-            txtPriceProduct.text = price.toString()
-            txtDescriptionProducto.text = description
-            rtgValuationProduct.rating = valuation!!
-            txtCalificationProduct.text = calification.toString()
-            Picasso.get().load(image).into(imgProduct)
-            txtPriceCuotaProducto.text = quota.toString()
-        }*/
 
         //With SafeArgs
         val title = args.title
@@ -79,6 +61,10 @@ class DetailFragment : Fragment() {
         txtCalificationProduct.text = calification.toString()
         Picasso.get().load(image).into(imgProduct)
         txtPriceCuotaProducto.text = "%.2f".format(quota)
+
+        btn_add.setOnClickListener {
+            Toast.makeText(activity, getString(R.string.textAddBtn),Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
